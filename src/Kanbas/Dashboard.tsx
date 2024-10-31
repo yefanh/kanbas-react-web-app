13aab5bc76d26174f1ffb48e610e3143149f2a65
Yefan He <hyf528@outlook.com>
2024-10-31 18:39:37 -0400

    bug when doing 3.2

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -1,7 +1,9 @@
+//src/Kanbas/Dashboard.tsx
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import * as db from "./Database";
 
-// 定义课程的类型
+// define the Course interface
 interface Course {
   _id: string;
   name: string;
@@ -14,15 +16,63 @@ interface Course {
 }
 
 export default function Dashboard() {
-  const courses: Course[] = db.courses;  // 为 courses 数据添加类型
+
+  const [courses, setCourses] = useState<any[]>(db.courses);
+
+  const [course, setCourse] = useState<any>({   // convert course into a state variable so we can change it and force a redraw of the UI
+    _id: "0", name: "New Course", number: "New Number",
+    startDate: "2023-09-10", endDate: "2023-12-15",
+    image: "/images/reactjs.jpg", description: "New Description"
+  });
+
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
+
+  const addNewCourse = () => {
+    const newCourse = { ...course,
+                        _id: new Date().getTime().toString() };
+    setCourses([...courses, { ...course, ...newCourse }]);
+  };
+
+  // add deleteCourse event handler accepting ID of course to remove by filtering out the course by its ID
+  const deleteCourse = (courseId: string) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
 
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+      <h5>New Course
+          <button className="btn btn-primary float-end"
+                  id="wd-add-new-course-click"
+                  onClick={addNewCourse} > Add 
+          </button>
+
+          <button className="btn btn-warning float-end me-2"
+                onClick={updateCourse} id="wd-update-course-click">
+          Update
+          </button>
+      </h5><br />   
+
+      {/* add input element for each of fields in course state variable */}
+      <input    value={course.name} className="form-control mb-2" 
+                onChange={(e) => setCourse({ ...course, name: e.target.value }) } />
+      <textarea value={course.description} className="form-control"
+                onChange={(e) => setCourse({ ...course, description: e.target.value }) } />
+
       <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
       <div id="wd-dashboard-courses" className="row">
         <div className="row row-cols-1 row-cols-md-5 g-4">
-          {courses.map((course: Course) => (  // 为 course 添加类型声明
+          {courses.map((course: Course) => (  // add type declaration for course
             <div key={course._id} className="wd-dashboard-course col" style={{ width: "300px" }}>
               <div className="card rounded-3 overflow-hidden">
                 <Link to={`/Kanbas/Courses/${course._id}/Home`}
@@ -33,7 +83,33 @@ export default function Dashboard() {
                       {course.name} </h5>
                     <p className="wd-dashboard-course-title card-text overflow-y-hidden" style={{ maxHeight: 100 }}>
                       {course.description} </p>
-                    <button className="btn btn-primary"> Go </button>
+
+                    <button className="btn btn-primary" > Go </button>
+                    
+                    
+
+                    {/* add Delete button next to the course's name to invoke deleteCourse when clicked 
+                    passing the course's ID and preventing the Link's default behavior to navigate to 
+                    Course Screen */}
+                    <button onClick={(event) => {
+                      event.preventDefault();
+                      deleteCourse(course._id);
+                    }} className="btn btn-danger float-end"
+                    id="wd-delete-course-click">
+                    Delete
+                    </button>
+
+                    {/*  next to the Delete button add an Edit button to copy the course to be edited 
+                    into the form so we can edit it. Prevent default to navigate to Course screen */}
+                    <button id="wd-edit-course-click"
+                      onClick={(event) => {
+                        event.preventDefault();
+                        setCourse(course);
+                      }}
+                      className="btn btn-warning me-2 float-end" >
+                      Edit
+                    </button>
+
                   </div>
                 </Link>
               </div>
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -5,8 +5,33 @@ import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
 import Courses from "./Courses";
 import "./styles.css";
+import * as db from "./Database";
+import { useState } from "react";
 
 export default function Kanbas() {
+  const [courses, setCourses] = useState<any[]>(db.courses);
+  const [course, setCourse] = useState<any>({
+    _id: "1234", name: "New Course", number: "New Number",
+    startDate: "2023-09-10", endDate: "2023-12-15", description: "New Description",
+  });
+  const addNewCourse = () => {
+    setCourses([...courses, { ...course, _id: new Date().getTime().toString() }]);
+  };
+  const deleteCourse = (courseId: any) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
+
   return (
     <div id="wd-kanbas">
       <KanbasNavigation />
@@ -15,10 +40,24 @@ export default function Kanbas() {
             <Route path="/" element={<Navigate to="Account" />} />  {/* show account page by default */}
             <Route path="/Account/*" element={<Account />} />  {/* load account component */}
             <Route path="/Dashboard" element={<Dashboard />} />  {/* load dashboard component */}
+
+            <Route path="/Courses" 
+              element={ 
+                <Dashboard
+                  courses={courses}
+                  course={course}
+                  setCourse={setCourse}
+                  addNewCourse={addNewCourse}
+                  deleteCourse={deleteCourse}
+                  updateCourse={updateCourse}
+                />
+              } 
+            />  
+
             {/* Add Courses route, ":cid" is a dynamic parameter that represents the course 
             ID means When a user clicks on a different course, a different course page is displayed 
             based on this ID*/}
-            <Route path="/Courses/:cid/*" element={<Courses />} />
+            <Route path="/Courses/:cid/*" element={<Courses courses={courses}/>} />
             <Route path="/Calendar" element={<h1>Calendar</h1>} />
             <Route path="/Inbox" element={<h1>Inbox</h1>} />
           </Routes>
